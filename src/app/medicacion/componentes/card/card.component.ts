d65c194532a70e94ae05678aa598d7ed2bd8712e
masacr3(leo R.) <leorhcp2030@gmail.com>
2022-03-11 18:23:08 -0300

    se agregaron focus info

--- src/app/medicacion/componentes/card/card.component.ts
+++ src/app/medicacion/componentes/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { RemediosI } from '../../models/remedios';
 
 @Component({
@@ -9,10 +9,12 @@ import { RemediosI } from '../../models/remedios';
 export class CardComponent implements OnInit {
 
   @Input() medicamento!: RemediosI;
+  @ViewChild('principal') card!: ElementRef;
   info = "Info"
   focus = false
+  posicionAnterior : number | undefined
 
-  constructor() { }
+  constructor(private render : Renderer2) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +23,29 @@ export class CardComponent implements OnInit {
     let clase = 'redondo mid-12-2 '
     if (cant <= 2) return clase + "peligro"
     return clase + (cant >= 10 ? "ok" : "atento")
+  }
+
+  action() : void{
+    const box = this.card.nativeElement
+    this.focus = !this.focus
+
+    if ( this.focus ){
+      const currentTop = box.getBoundingClientRect().top
+      const currentCross = window.scrollY
+      this.posicionAnterior = currentCross
+      this.render.addClass(box, "full-card")
+      console.log("cross +"+currentCross)
+      window.scroll(0, currentTop + currentCross - 10)
+      this.info = "Cerrar"
+
+      console.log("cross postmov+"+window.scrollY)
+    }
+    else{
+      this.render.removeClass(box, "full-card")
+      this.info = "Info"
+      console.log("anterior +"+this.posicionAnterior)
+      window.scroll(0, this.posicionAnterior === undefined ? 0 : this.posicionAnterior)
+    }
   }
 
 }
